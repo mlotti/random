958a41cf983c6dfab86b87edcbcc259fc7717e3a
mlotti <mikko.lotti@cern.ch>
2024-08-12 11:00:06 +0200

    added random password generator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# random
+
+For now, do 
+
+´´´g++ -std=c++14 -I /usr/local/opt/boost/include randoms.cpp -L /usr/local/opt/boost/lib -lboost_random´´´
+
+
+
---
+++ randoms.cpp
@@ -0,0 +1,18 @@
+#include <boost/random/random_device.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <iostream>
+
+int main() {
+    std::string chars(
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "1234567890"
+        "!@#$%^&*()"
+        "`~-_=+[{]}\\|;:'\",<.>/? ");
+    boost::random::random_device rng;
+    boost::random::uniform_int_distribution<> index_dist(0, chars.size() - 1);
+    for(int i = 0; i < 8; ++i) {
+        std::cout << chars[index_dist(rng)];
+    }
+    std::cout << std::endl;
+}
